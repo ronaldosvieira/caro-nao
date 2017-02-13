f179eb2af84afa2aeba3ac46c78bde6ce2a97b83
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-02-13 19:59:40 -0200

    Edição de veículos de caronas

---
+++ WebContent/views/carona/editar.jsp
@@ -0,0 +1,85 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="pt_BR">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Caronão</title>
+
+    <!-- Bootstrap -->
+    <link href="${pageContext.request.contextPath}/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- CSS do caronão -->
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+  <%@ page import = "util.RecordSet" %>
+  <%@ page import = "util.Row" %>
+  
+  <% String erro = (String) request.getAttribute("erro"); %>
+  <% RecordSet veiculos = (RecordSet) request.getAttribute("veiculos"); %>
+  <% RecordSet carona = (RecordSet) request.getAttribute("carona"); %>
+  
+	<header class="text-center">
+		<h1>
+			Caro<span style="color: #DDD;">,</span>não
+		</h1>
+	 </header>
+	<div class="container">
+		<div class="col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6
+			col-lg-offset-4 col-lg-4 text-center">
+			
+			<% if (erro != null) { %>
+				<div class="text-center text-danger">
+					Erro: <%= erro %>
+				</div>
+			<% } %>
+			
+			<h4>Editar veículo de carona</h4>
+			
+			<form action="${pageContext.request.contextPath}/carona/editar?id=<%= carona.get(0).getInt("id") %>"
+				method="post" class="form-horizontal">
+				<div class="form-group">
+					<label for="veiculo_id">Veículo</label>
+					<select name="veiculo_id" class="form-control" required>
+					<option value="" class="text-center">
+						(Escolha um veículo)
+					</option>
+					<% for (Row veiculo : veiculos) { %>
+						<option value="<%= veiculo.getInt("id") %>" 
+							class="text-center" required
+							<%= veiculo.getInt("id") == 
+								carona.get(0).getInt("veiculo_id")?
+										"selected" : "" %>>
+							<%= veiculo.getString("modelo") %>
+						</option>
+					<% } %>
+					</select>
+				</div>
+				
+				<div class="form-group">
+					<button class="btn btn-default btn-block" 
+						type="submit">Salvar</button>
+					<a href="${pageContext.request.contextPath}/carona/ver?id=<%= carona.get(0).getInt("id") %>" 
+						class="btn btn-block btn-link">Voltar</a>
+				</div>
+			</form>
+		</div>
+	</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="${pageContext.request.contextPath}/js/jquery-3.1.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="${pageContext.request.contextPath}/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ src/servico/carona/EditarCarona.java
@@ -0,0 +1,122 @@
+package servico.carona;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dominio.CaronaModule;
+import dominio.UsuarioModule;
+import excecoes.CEPInvalidoException;
+import excecoes.CaronaNaoAutorizadaException;
+import excecoes.CaronaNaoExisteException;
+import excecoes.DataInvalidaException;
+import excecoes.GrupoNaoAutorizadoException;
+import excecoes.ServicoDeEnderecosInacessivelException;
+import excecoes.UsuarioNaoLogadoException;
+import excecoes.VeiculoComMenosVagasException;
+import excecoes.VeiculoJaSelecionadoException;
+import excecoes.VeiculoNaoExisteException;
+import servico.autenticacao.Autenticacao;
+import util.RecordSet;
+
+@WebServlet("/carona/editar")
+public class EditarCarona extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public EditarCarona() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String idCarona = (String) request.getParameter("id");
+		
+		try {
+			RecordSet usuario = Autenticacao.autenticar(request, response);
+			
+			UsuarioModule um = new UsuarioModule();
+			
+			RecordSet carona = um.validarCarona(usuario.get(0).getInt("id"), 
+					Integer.parseInt(idCarona));
+			um.validarDonoCarona(usuario.get(0).getInt("id"), 
+					Integer.parseInt(idCarona));
+			
+			RecordSet veiculos = um.listarVeiculos(usuario.get(0).getInt("id"));
+		
+			request.setAttribute("usuario", usuario);
+			request.setAttribute("carona", carona);
+			request.setAttribute("veiculos", veiculos);
+			
+			RequestDispatcher rd = 
+					request.getRequestDispatcher("../views/carona/editar.jsp");
+			rd.forward(request, response);
+		} catch (UsuarioNaoLogadoException e) {
+			response.sendRedirect(request.getContextPath() + "");
+		} catch (ClassNotFoundException | SQLException e) {
+			response.getWriter().append("Erro ao acessar o banco de dados");
+			e.printStackTrace();
+		} catch (NumberFormatException | CaronaNaoAutorizadaException
+				| VeiculoNaoExisteException | CaronaNaoExisteException e) {
+			response.sendRedirect(request.getContextPath() + "/dashboard");
+		} 
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String idCarona = (String) request.getParameter("id");
+		String idVeiculo = (String) request.getParameter("veiculo_id");
+
+		try {
+			RecordSet usuario = Autenticacao.autenticar(request, response);
+			
+			CaronaModule cm = new CaronaModule();
+			UsuarioModule um = new UsuarioModule();
+			
+			RecordSet carona = um.validarCarona(usuario.get(0).getInt("id"), 
+					Integer.parseInt(idCarona));
+			
+			um.validarDonoCarona(usuario.get(0).getInt("id"), 
+					Integer.parseInt(idCarona));
+			
+			RecordSet veiculos = um.listarVeiculos(usuario.get(0).getInt("id"));
+			
+			request.setAttribute("usuario", usuario);
+			request.setAttribute("carona", carona);
+			request.setAttribute("veiculos", veiculos);
+	
+			cm.atualizarVeiculoDaCarona(Integer.parseInt(idCarona),
+					Integer.parseInt(idVeiculo));
+			
+			response.sendRedirect(request.getContextPath() + "/carona/ver?id=" + idCarona);
+		} catch (ClassNotFoundException | SQLException e) {
+			response.getWriter().append("Erro ao acessar o banco de dados");
+			e.printStackTrace();
+		} catch (UsuarioNaoLogadoException e) {
+			response.sendRedirect(request.getContextPath() + "");
+		} catch (NumberFormatException | CaronaNaoAutorizadaException
+				| VeiculoNaoExisteException | CaronaNaoExisteException e) {
+			response.sendRedirect(request.getContextPath() + "/dashboard");
+		} catch (VeiculoJaSelecionadoException e) {
+			request.setAttribute("erro", 
+					"O veículo informado já foi selecionado para uma "
+					+ "carona neste dia e horário.");
+			
+			RequestDispatcher rd = 
+					request.getRequestDispatcher("../views/carona/editar.jsp");
+			rd.forward(request, response);
+		} catch (VeiculoComMenosVagasException e) {
+			request.setAttribute("erro", 
+					"Não é possível trocar para um veículo "
+					+ "com menos vagas.");
+			
+			RequestDispatcher rd = 
+					request.getRequestDispatcher("../views/carona/editar.jsp");
+			rd.forward(request, response);
+		}
+	}
+}
