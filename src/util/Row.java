7f2391357796edd062a2404c8286a3d4dcb5d3dd
Ronaldo Vieira <ronaldo.vieira@ufrrj.br>
2017-02-14 15:45:30 -0200

    Alteração dos tipos de retorno em Row

--- src/util/Row.java
+++ src/util/Row.java
@@ -1,30 +1,25 @@
 package util;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.HashMap;
 
 public class Row extends HashMap<String, Object> {
 	private static final long serialVersionUID = 884220558870106730L;
 	
-	public int getInt(String column) {
-		return (int) this.get(column);
+	public Integer getInt(String column) {
+		return (Integer) this.get(column);
 	}
 	
-	public float getFloat(String column) {
-		return (float) this.get(column);
+	public Float getFloat(String column) {
+		return (Float) this.get(column);
 	}
 	
-	public boolean getBoolean(String column) {
-		return (boolean) this.get(column);
+	public Boolean getBoolean(String column) {
+		return (Boolean) this.get(column);
 	}
 	
 	public String getString(String column) {
 		return (String) this.get(column);
-	}
-	
-	public Date getDate(String column) {
-		return (Date) this.get(column);
 	}
 	
 	public Timestamp getTimestamp(String column) {
