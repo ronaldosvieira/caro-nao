5699c4c2491abf9f9024b92468a3d51dde77915b
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-02-14 22:27:17 -0200

    Criação de dados para teste ao banco

---
+++ test-data.sql
@@ -0,0 +1,22 @@
+insert into grupo (nome, descricao, regras, limite_avaliacoes_negativas)
+values ('Grupo de carona #1', 'Carona po', 'Nenhuma', 3);
+
+insert into usuario (nome, email, telefone)
+values ('Rodrigo', 'rodrigovicente@gmail.com', '90909090'),
+	('Ronaldo', 'ronaldo.vieira@ufrrj.br', '89898989');
+	
+insert into grupo_usuario (grupo_id, usuario_id)
+values (1, 1);
+
+insert into veiculo (modelo, placa, cor, vagas, usuario_id)
+values ('Fiat Uno', 'SOD-4394', 'Azul marinho', 4, 1);
+
+insert into logradouro (cep, estado, cidade, distrito, endereco, numero)
+values ('26170230', 'RJ', 'Belford Roxo', 'Pauline', 'Rua Acre', '28'),
+	('29238237', 'RJ', 'Rio de Janeiro', 'Cidade Universitária', 'Rua Helio de Almeida', 's/n');
+
+insert into carona (veiculo_id, dia_horario, logradouro_origem_id, logradouro_destino_id)
+values (1, NOW(), 1, 2);
+
+insert into carona_usuario (usuario_id, carona_id, logradouro_id)
+values (1, 1, 2);
